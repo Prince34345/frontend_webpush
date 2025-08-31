d77a73a7503c239a1b1d28924bd54786df0d6a7c
Prince34345 <priyanshudhawan55@gmail.com>
2025-08-31 23:41:31 +0530

    minor issue fix

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,4 +1,4 @@
-export const API_BASE = "http://localhost:8000";
+export const API_BASE = process.env.REACT_APP_API_BASE;
 
 export async function getVapidPublicKey() {
   const res = await fetch(`${API_BASE}/vapid/public`);
